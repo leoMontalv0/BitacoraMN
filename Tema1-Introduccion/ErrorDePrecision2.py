98c49718ac1014f379fe6224a271026cf2c10f64
24030347-Fatima <24030347@itesa.edu.mx>
2026-06-02 23:43:38 -0600

    Fati agrego error de precision

---
+++ Tema1-Introduccion/ErrorDePrecision2.py
@@ -0,0 +1,11 @@
+# ErrorDeRedondeo.py
+
+numero_real = 3.14159265
+
+numero_redondeado = round(numero_real, 2)
+
+error = abs(numero_real - numero_redondeado)
+
+print("Número real:", numero_real)
+print("Número redondeado:", numero_redondeado)
+print("Error de redondeo:", error)
